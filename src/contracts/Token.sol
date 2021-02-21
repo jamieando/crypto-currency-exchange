139ae45897c2638d3bc867ed4d870101113ebede
jamieando <jamie_anderson81@hotmail.com>
2021-02-21 11:30:37 +1100

    Smart contract tests

--- src/contracts/Token.sol
+++ src/contracts/Token.sol
@@ -1,5 +1,12 @@
 pragma solidity ^0.5.0;
 
 contract Token {
-    string public name = 'My Name';
+    string public name = 'Novus Token';
+    string public symbol = 'NOV';
+    uint256 public decimals = 18;
+    uint256 public totalSupply;
+
+    constructor() public {
+        totalSupply = 1000000 * (10 ** decimals);
+    }
 }
